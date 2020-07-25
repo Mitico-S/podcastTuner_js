0829caf7e722cf50470986580a0aa9b9299803b3
João Pedro Alves <jotalmeida007@hotmail.com>
2020-07-25 12:35:26 -0300

    endding project

---
+++ js/podcasts.js
@@ -0,0 +1,9 @@
+var realPodcasts = [
+  {
+    id: '88.5',
+    title: 'Syntax',
+    description:
+      '[Inglês] Desenvolvimento Java script em geral. Apresentado por Wes Bos Scott Tolisnki',
+    img: '1.png',
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//
+//
+
 var inputCurrentFrequency = document.querySelector('#inputCurrentFrequency');
 var rangeFrequency = document.querySelector('#rangeFrequency');
 var divPodcast = document.querySelector('#divPodcast');
@@ -6,8 +9,6 @@ function start() {
   rangeFrequency.addEventListener('input', handleRangeValueChange);
 }
 
-start();
-
 function handleRangeValueChange(event) {
   var currentFrequency = event.target.value;
   inputCurrentFrequency.value = currentFrequency + ' Mhz';
@@ -15,4 +16,44 @@ function handleRangeValueChange(event) {
   findPodcastFrom(currentFrequency);
 }
 
-function findPodcastFrom(frequency) {}
+function findPodcastFrom(frequency) {
+  var foundPodcast = null;
+
+  console.log(realPodcasts);
+  for (var i = 0; i < realPodcasts.length; i++) {
+    var currentPodcast = realPodcasts[i];
+
+    if (realPodcasts[i].id === frequency) {
+      foundPodcast = currentPodcast;
+
+      break;
+    }
+  }
+
+  if (foundPodcast) {
+    renderPodcast(foundPodcast);
+  } else {
+    divPodcast.innerHTML = '<p>Nenhum podcast encontrado</p>';
+  }
+}
+
+function renderPodcast(podcast) {
+  divPodcast.innerHTML = '';
+
+  var img = document.createElement('img');
+  img.src = './img/' + podcast.img;
+  img.alt = 'Podcast' + podcast.title;
+  img.title = 'Podcast' + podcast.title;
+
+  var title = document.createElement('h2');
+  title.textContent = podcast.title;
+
+  var description = document.createElement('h2');
+  description.textContent = podcast.description;
+
+  divPodcast.appendChild(img);
+  divPodcast.appendChild(title);
+  divPodcast.appendChild(description);
+}
+
+start();
